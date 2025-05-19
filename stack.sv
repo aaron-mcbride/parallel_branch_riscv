67f1733f782b4fc2e149995815216f1c632c9aef
Aaron McBride <151467777+aaron-mcbride@users.noreply.github.com>
2025-05-18 21:10:37 -0700

    Started target predictor

---
+++ stack.sv
@@ -0,0 +1,14 @@
+`ifndef __STACK_SV__
+`define __STACK_SV__
+
+// Stack module
+module stack #(
+  parameter int size = 32,
+  parameter type data_type = logic [31:0]
+) (
+  input logic clk,
+  input logic rst,
+  input logic 
+)
+
+`endif // __STACK_SV__
--- targ_pred.sv
+++ targ_pred.sv
@@ -3,44 +3,51 @@
 `ifndef __TARG_PRED_SV__
 `define __TARG_PRED_SV__
 
-`define TARG_PRED_TYPE_SIMPLE_BTB
-
 module targ_pred #(
   parameter int s_pipe_cnt = 3,
+  parameter int table_cnt = 2,
   parameter int table_size = 32,
-  parameter int table_width = 5,
-  parameter int hist_width = 2,
-  parameter int 
+  parameter int table_width = 3,
+  parameter int hist_thresh = 1,
 ) (
   input logic clk,
   input logic rst,
   input logic en,
+  input logic [($clog2(table_cnt) - 1):0] table_index,
   input core::targ_pred_req_t req [s_pipe_cnt],
   input core::targ_pred_fb_t fb,
   output core::targ_pred_rsp_t rsp [s_pipe_cnt]
 );
 
-  `ifdef TARG_PRED_TYPE_SIMPLE_BTB
+  // Numeric types for history and
+  typedef logic [(hist_width - 1):0] hist_value_t;
 
-    // Target buffer
-    logic [sys::addr_t] targ_buff [table_size];
+  // Table entry struct type
+  typedef struct packed {
+    hist_value_t hist_value [table_width];
+    sys::addr_t targ_addr [table_width];
+    bool valid;
+  } target_info_t;
+    
+  // Target prediction tables
+  target_info_t targ_pred_table [table_cnt][table_size];
 
-    // Prediction request response logic
-    always @(*) begin
-      for (int i = 0; i < s_pipe_cnt; i++) begin
-        rsp[i] = core::targ_pred_rsp_rst;
-        if (en && req.valid) begin
-          rsp[i].valid[0] = true;
-          rsp[i].pred = targ_buff[req[i].addr[($clog2(table_size) - 1):0]];
+  // Prediction request response logic
+  target_info_t cur_targ_info [s_pipe_cnt];
+  always @(*) begin
+    for (int i = 0; i < s_pipe_cnt; i++) begin
+      rsp[i] = core::targ_pred_rsp_rst;
+      if (en && req.valid) begin
+        cur_targ_info[i] = targ_pred_table[table_index]
+            [req[i].addr[($clog2(table_size) - 1):0]];
+        if (cur_targ_info[i].valid) begin
+          for (int j = 0; j < table_width; j++) begin
+            if (cur_targ_info[i].hist_value)
+          end
         end
       end
     end
-
-  `else
-
-  `endif
-
-
+  end
 
 endmodule
 
